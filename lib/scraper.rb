2b7923b167e1dd87d5f507082f91c4f7b186df96
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-21 00:35:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,18 @@ class Scraper
   
   
   def self.scrape_index_page(index_url)
+   page = Nokogiri::HTML(open(index_url))
     students = {}
-    Nokogiri::HTML(open(index_url))
-    
+    students_array = []
+    page.css("div.student-card").each do |student|
+      students = {
+      :name => student.css("a div.card-text-container h4.student-name").text,
+      :location => student.css("a div.card-text-container p.student-location").text,
+      :profile_url => student.css("a").attribute("href").value
+      }
+    students_array << students 
+    end 
+    students_array
   end
 
   def self.scrape_profile_page(profile_url)
