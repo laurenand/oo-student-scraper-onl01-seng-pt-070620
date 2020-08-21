ac1a39da4a74de8f5c88c8484f24af1b73d18214
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-21 00:28:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,15 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
-
+  
+  
+  
   def self.scrape_index_page(index_url)
-    
+    students = {}
+    Nokogiri::HTML(open(index_url))
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
